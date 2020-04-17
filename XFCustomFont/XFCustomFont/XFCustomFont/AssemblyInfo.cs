203f182a138abe76059aedc8576899fd7c68744e
Sebastian Jensen <apps@tsjdev-apps.de>
2020-04-17 22:48:09 +0200

    - add solution and add custom font

---
+++ XFCustomFont/XFCustomFont/XFCustomFont/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("IndieFlower-Regular.ttf", Alias = "IndieFlower")]
